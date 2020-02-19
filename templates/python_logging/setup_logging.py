10eaeab3ca072b9d1d6e3a8fb286284b7ff1394e
neaxi <25987075+neaxi@users.noreply.github.com>
2020-02-19 08:57:08 +0100

    improved datetime to comply with ISO8601

--- templates/python_logging/setup_logging.py
+++ templates/python_logging/setup_logging.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import os
 
-logScreenLevel = 'INFO'
-logFileLevel = 'DEBUG'
+log_screen_level = 'INFO'
+log_file_level = 'DEBUG'
+date_format = '%Y-%m-%dT%H:%M:%S%z'    # ISO 8601!!!
+
 
 RUNTIME_LOG = os.path.join(os.path.dirname(__file__), 'last_execution.log')
 if not os.path.join(RUNTIME_LOG):
@@ -10,37 +12,41 @@ if not os.path.join(RUNTIME_LOG):
 
 DEFAULT_LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    # disable_existing_loggers – If specified as False, loggers which exist when this call is made are left enabled. 
+    'disable_existing_loggers': False,  
     'formatters': {
         'standard': {
-            'format': '[%(asctime)s] [%(levelname)s] %(message)s'
+            'format': '[%(asctime)s] [%(levelname)s] %(message)s',
+            'datefmt': date_format
         },
         'extensive': {
-            'format': '[%(asctime)s] [PID:%(process)d TID:%(thread)d] [%(levelname)s] [%(name)s] [%(funcName)s():%(lineno)s] %(message)s'
+            # all available attributes - https://docs.python.org/3/library/logging.html#logrecord-attributes
+            'format': '[%(asctime)s] [PID:%(process)d TID:%(thread)d] [%(levelname)s] [%(name)s] [%(funcName)s():%(lineno)s] %(message)s',
+            'datefmt': date_format
         }
     },
     'handlers': {
         'default': {
-            'level': logScreenLevel,
+            'level': log_screen_level,
             'formatter': 'standard',
             'class': 'logging.StreamHandler',
         },
         'file': {
-            'level': logFileLevel,
+            'level': log_file_level,
             'formatter': 'standard',
             'class': 'logging.FileHandler',
             'filename': RUNTIME_LOG,
             'mode': 'w',
         },
         'default_extensive': {
-            'level': logScreenLevel,
+            'level': log_screen_level,
             'formatter': 'extensive',
             'class': 'logging.StreamHandler',
         }
     },
     'loggers': {
         '': {
-            'level': logFileLevel,
+            'level': log_file_level,
             'handlers': ['default', 'file'],
             'propagate': True
         },
